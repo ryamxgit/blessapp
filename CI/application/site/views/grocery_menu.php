6ad318b1082bf0e90e8ad2d9c2afcfc5a8c63f50
ryamxgit <rodri.guevara@gmail.com>
2017-02-19 19:41:15 -0300

    Archivos que quedaron en la cola

---
+++ CI/application/site/views/grocery_menu.php
@@ -0,0 +1,15 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<span>Opciones</span>
+			<div class="btn-group btn-group-lg">
+			{menu}	 
+				<button class="btn btn-default" type="button">
+					<a href="{link}"><span>{titulo}</span></a>
+				</button> 
+			{/menu}
+			</div>
+		</div>
+	</div>
+</div>
+
